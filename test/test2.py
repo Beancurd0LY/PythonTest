c3a0f6eea6f7789615e5e5aa5494ce23e55024c8
刘遥 <175247@sh.lianjia.com>
2018-01-29 18:30:50 +0800

    获取文件名

--- test/test2.py
+++ test/test2.py
@@ -1,3 +1,9 @@
+#!/bin/env python
+# coding=utf-8
+import os
+
+
+
 def logger(func):
     def inner(*args, **kwargs): #1
         print "Arguments were: %s, %s" % (args, kwargs)
@@ -27,4 +33,20 @@ def log(func):
 def now():
     pass
 
-now()
+f1 = now()
+# f2 = now()
+# print f1,f2
+# print (f1 is f2)
+
+ignore_list = []
+# filepath = "D:\code-repo\TestPy\test"
+lst=os.listdir(os.getcwd())
+for name in lst:
+    if (os.path.isfile(name) and name.endswith(".py") and name!="__init__.py"):
+        ignore_list.append(name)
+print ignore_list
+print('\n'.join(c for c in lst if os.path.isfile(c) and c.endswith('.py')))
+print dir(ignore_list[1]) 
+
+
+
